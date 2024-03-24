5be69e6c76cc0232cc9f65502e4b08775d36d42f
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2024-03-24 16:30:27 -0600

    Create tst.py

---
+++ tst.py
@@ -0,0 +1,12 @@
+# import pandas as pd
+# import numpy as np
+
+def main():
+    print("Tests!")
+    even_numbers = list(range(2,11,2))
+    print(even_numbers)
+
+password = "12345" # put sensitive info by mistake
+
+if __name__ == "__main__":
+    main()
